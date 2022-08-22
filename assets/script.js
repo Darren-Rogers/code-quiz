102725135a4e203e6f8f1703d11b61fcb902ac05
Sreggie <darren.rogers06@gmail.com>
2022-08-22 17:07:08 -0400

    added html elements start, questions and submit

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var startEl = document.querySelector('#start')
+var quizEl = document.querySelector('#quiz')
+var endEl = document.querySelector('#end')
+var titleEl = document.querySelector('#title')
