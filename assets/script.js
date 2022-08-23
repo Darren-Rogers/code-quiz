6ce03a1f71710dfd43d1ebc6368db6f5443c070e
Sreggie <darren.rogers06@gmail.com>
2022-08-23 17:55:27 -0400

    added working timer and highscore screen

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
 var startElement = document.querySelector('#start')
 var quizElement = document.querySelector('#quiz')
 var endElement = document.querySelector('#end')
+var scoreElement = document.querySelector('#high')
 var titleElement = document.querySelector('#title')
 var questionsElement = document.querySelector('#questions')
 var startButton = document.querySelector("#startButton")
 var submitButton = document.querySelector("#endButton")
+var enterInitials =document.querySelector('#enterInput')
+var timer;
+var timerElement = document.querySelector('.time')
+var seconds = 60;
 var questions = [
     {
         text: 'When was the Magna Carta signed?',
@@ -28,25 +33,33 @@ var questions = [
     }
 ]
 var questionOrder = 0
-state = 'start';
+state = 'end';
 // state changes the view to the start, quiz questions or the end
 function currentSection() {
     if (state === 'start') {
         startElement.style.display = 'block';
         quizElement.style.display = 'none';
         endElement.style.display = 'none';
+        scoreElement.style.display = 'none';
     }
     if (state === 'quiz') {
         startElement.style.display = 'none';
         quizElement.style.display = 'block';
         endElement.style.display = 'none';
         displayQuestion()
+        scoreElement.style.display ='none';
     }
     if (state === 'end') {
         startElement.style.display = 'none';
         quizElement.style.display = 'none';
         endElement.style.display = 'block';
-
+        scoreElement.style.display = 'none';
+    }
+    if (state === 'high') {
+        startElement.style.display = 'none';
+        quizElement.style.display = 'none';
+        endElement.style.display = 'none';
+        scoreElement.style.display ='block';
     }
 };
 currentSection()
@@ -78,14 +91,6 @@ function displayQuestion(){
         questionsElement.append(buttonElement)
     }
 };
-startButton.addEventListener('click',function(){
-    state='quiz'
-    currentSection();
-}
-);
-submitButton.addEventListener('click',function(){
-    
-});
 //checks if the answer is correct and alerts a corresponding alert i.e correct or wrong
 function checkAnswer(answer){
     if(answer == questions[questionOrder].correct){
@@ -93,5 +98,50 @@ function checkAnswer(answer){
     }
     else{
         alert('Wrong!')
+        seconds -= 10;
     }
 };
+timerElement.textContent = seconds;
+function setTime(){
+    timerElement.textContent = seconds;
+    var timerInterval = setInterval(function(){
+        seconds--;
+        timerElement.textContent = seconds;
+        if(seconds === 0 ){
+            state = 'end',
+            clearInterval(timerInterval);
+            sendMessage()
+            currentSection()
+        }
+        if(state == 'end'){
+            clearInterval(timerInterval)
+            score = seconds
+        }
+    },1000)
+};
+function sendMessage(){
+    timerElement.textContent = 'Time has run out'  
+};
+function highScore(){
+    var printInitials = localStorage.getItem(initials)
+    var printScore = localStorage.getItem(timeLeft)
+    localStorage.getItem(printScore)
+    
+
+};
+startButton.addEventListener('click',function(){
+    state='quiz'
+    currentSection();
+    setTime()
+}
+);
+submitButton.addEventListener('click',function(){
+    state = 'high'
+    initials = enterInitials.value
+    localStorage.setItem('Initials',initials)
+    timeLeft = JSON.stringify(seconds)
+    localStorage.setItem('time',timeLeft)
+    currentSection()
+    console.log(initials)
+    console.log(timeLeft)
+});
