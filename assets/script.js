5948f2bd07588f39a6c4b64c5e3300196216eca1
Sreggie <darren.rogers06@gmail.com>
2022-08-22 21:09:04 -0400

    added state and logic to go through questions

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,92 @@
-var startEl = document.querySelector('#start')
-var quizEl = document.querySelector('#quiz')
-var endEl = document.querySelector('#end')
-var titleEl = document.querySelector('#title')
+var startElement = document.querySelector('#start')
+var quizElement = document.querySelector('#quiz')
+var endElement = document.querySelector('#end')
+var titleElement = document.querySelector('#title')
+var questionsElement = document.querySelector('#questions')
+var startButton = document.querySelector("#startButton")
+var submitButton = document.querySelector("#endButton")
+var questions = [
+    {
+        text: 'When was the Magna Carta signed?',
+        choices: ['1520', '1215', '1812', '1776'],
+        correct: '1215',
+    },
+    {
+        text: 'What British king was publicly beheaded?',
+        choices: ['King John', 'King Charles', 'King James', 'King George'],
+        correct: 'King Charles',
+    },
+    {
+        text: 'What battle was Napoleon defeated at?',
+        choices: ['Waterloo', 'Trafalgar', 'Lubeck', 'Borodino'],
+        correct: 'Waterloo',
+    },
+    {
+        text: 'Who is considered to be "The Renaissance Man"?',
+        choices: ['Vincent Van Gough', 'Michealangelo', 'Raphael', 'Leonardo da Vinci'],
+        correct: 'Leonardo da Vinci'
+    }
+]
+var questionOrder = 0
+state = 'start';
+function currentSection() {
+    if (state === 'start') {
+        startElement.style.display = 'block';
+        quizElement.style.display = 'none';
+        endElement.style.display = 'none';
+    }
+    if (state === 'quiz') {
+        startElement.style.display = 'none';
+        quizElement.style.display = 'block';
+        endElement.style.display = 'none';
+        displayQuestion()
+    }
+    if (state === 'end') {
+        startElement.style.display = 'none';
+        quizElement.style.display = 'none';
+        endElement.style.display = 'block';
+
+    }
+}
+currentSection()
+function nextQues(event){
+    questionOrder++
+    questionsElement.innerHTML = null
+    if(questionOrder < questions.length){
+
+        displayQuestion()
+    }
+    else{
+        state = 'end'
+        currentSection()
+    }
+}
+function displayQuestion(){
+    state='quiz'
+    var questionH2 = document.createElement('h2');
+    questionH2.textContent = questions[questionOrder].text
+    questionsElement.append(questionH2)
+    for(i=0; i < questions[questionOrder].choices.length ; i++){
+        var buttonElement = document.createElement('button')
+        buttonElement.textContent = questions[questionOrder].choices[i]
+        buttonElement.addEventListener('click', nextQues)
+        questionsElement.append(buttonElement)
+    }
+}
+startButton.addEventListener('click',function(){
+    state='quiz'
+    currentSection();
+    
+}
+);
+submitButton.addEventListener('click',function(){
+    
+});
+function checkAnswer(answer){
+    if(answer == questions[questionOrder].correct){
+        alert('Correct!')
+    }
+    else{
+        alert('Wrong!')
+    }
+}
